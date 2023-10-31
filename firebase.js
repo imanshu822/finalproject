3a5179920d1863112b0f96675d35cf27b6a6857f
Anshu <imanshu822@gmail.com>
2023-11-01 00:44:52 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+const firebaseConfig = {
+  apiKey: "AIzaSyCWDDT0J11CZXCglIVTYXRjpC9--X62A94",
+  authDomain: "frankie-s-sports-images.firebaseapp.com",
+  projectId: "frankie-s-sports-images",
+  storageBucket: "frankie-s-sports-images.appspot.com",
+  messagingSenderId: "565582224329",
+  appId: "1:565582224329:web:b53eaecc880b505117a618"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
